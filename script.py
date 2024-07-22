bb418a78d83c6ce86478796b5eb0373eca0a9294
apal5 <apal5@binghamton.edu>
2024-07-22 14:00:24 -0400

    Sol

--- script.py
+++ script.py
@@ -1,11 +1,38 @@
 # Phase 1:
 # Write a script that retrieves info from the API and prints out all databases, their storage type, and their used storage.
 import requests
+import sys
 
 enpoint="https://6god8pgyzf.execute-api.us-west-2.amazonaws.com/databases"
 
 r=requests.get(enpoint)
+out = dict()
 if r.status_code==200:
     temp = r.json()['databases']
     for t in temp:
-        print(t['name'],t['storage_type'], t['storage_used'])
+        key=t['name']+"|"+t['storage_type']
+        if t['storage_used']:
+            value = float(t['storage_used'])/float(t['storage_size'])  * 100
+        else:
+            value=0
+        out[key]= value
+
+sorted_database = sorted(out, key=out.get, reverse=True)
+
+
+data = dict()
+for item in sorted_database:
+    temp = item.split('|')
+    name=temp[0]
+    data[name]= str(temp[1])+' '+str(out[item])
+# print(data)
+
+
+# Update your script to allow pulling the entire list, or just a single DB based on name
+print(sys.argv)
+if len(sys.argv)>1:
+    first = sys.argv[1]
+    print(data[first])
+else:
+    for item in data:
+        print(item, data[item])
